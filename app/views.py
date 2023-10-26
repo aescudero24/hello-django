5cfa3863cf1bdccf920058c3ffa806b1d67fc68c
aescudero <adrianescudero1305@gmail.com>
2023-10-26 14:35:12 -0500

    initial commit && assignment completed

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+
+# Create your views here.
+
+
+def hello_view(request: HttpRequest):
+    return HttpResponse("Hello, World!")
